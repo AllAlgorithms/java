e875872a25c2bb4db47f054abfd76bdfd0377a16
tadipatrisudheerreddy <tadipatrisudheerreddy@gmail.com>
2018-10-01 21:45:14 -0700

    Add Binary Tree traversals and regular problems

---
+++ BinaryTrees-traversals/BinaryTrees/algos/practice/BinaryTree.java
@@ -0,0 +1,52 @@
+package algos.practice;
+
+public class BinaryTree {
+	
+	 public static TreeNode mirrorTree(TreeNode node){
+		if(node == null) 
+			return node;
+		
+		TreeNode left = mirrorTree(node.left);
+		TreeNode right = mirrorTree(node.right);
+		node.left = right;
+		node.right = left;
+		 
+		return node;
+	}
+	 
+	 public static void main(String[] args){
+		 
+		TreeNode root = new TreeNode(8);
+		TreeNode.insert(root, new TreeNode(31));
+		TreeNode.insert(root, new TreeNode(45));
+		TreeNode.insert(root, new TreeNode(16));
+		TreeNode.insert(root, new TreeNode(24));
+		TreeNode.insert(root, new TreeNode(19));
+		TreeNode.insert(root, new TreeNode(29));
+		TreeNode.insert(root, new TreeNode(7));
+		
+		StringBuffer inorderbuf=new StringBuffer();
+		StringBuffer preorderbuf=new StringBuffer();
+		StringBuffer postorderbuf=new StringBuffer();
+		
+
+		TreeTraversal.getInorder(root,inorderbuf);
+		TreeTraversal.getPreorder(root, preorderbuf);
+		TreeTraversal.getPostorder(root, postorderbuf);
+		
+		System.out.println("Before mirroring,In order tree is: "+inorderbuf);
+		
+		BinaryTree.mirrorTree(root);
+		
+		StringBuffer inorderbufAfter = new StringBuffer();
+		TreeTraversal.getInorder(root, inorderbufAfter);
+		System.out.println("After mirroring,In order tree is: "+inorderbufAfter);
+		
+		System.out.println("Post order traversal:" + postorderbuf);
+		System.out.println("Pre order traversal:"+ preorderbuf);
+		
+		System.out.println("Nodes with no siblings:");
+		TreeWithNoSiblings.printOnlyLeaves(root);
+		
+	 }
+}
---
+++ BinaryTrees-traversals/BinaryTrees/algos/practice/SubTree.java
@@ -0,0 +1,60 @@
+package algos.practice;
+
+public class SubTree {
+
+	TreeNode root1, root2;
+	
+	public boolean isIdentical(TreeNode root1,TreeNode root2){
+		
+		//Since every null tree is also subset of main tree
+		if(root1==null&&root2==null)
+			return true;
+		//Need to check the null condition only once,since we are using recursion at second time it should not be null 
+		if(root1==null&&root2==null)
+			return false;		
+			
+	return (root1.value==root2.value)&& (isIdentical(root1.left,root2.left))
+			&&((isIdentical(root1.right,root2.right)) );
+	}
+	
+	public boolean isSubTree(TreeNode p, TreeNode q){
+		
+		//Since every null tree is also subset of main tree
+		if(q==null)
+			return true;
+		
+		if(p==null)
+			return false;
+		
+		if(isIdentical(p,q))
+			return true;
+	
+	return isSubTree(p.left,q)||isSubTree(p.right,q);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		SubTree tree = new SubTree();
+		
+		TreeNode root1 = new TreeNode(26);
+		root1.right    = new TreeNode(3);
+		root1.right.right = new TreeNode(3);
+		root1.left = new TreeNode(10);
+		root1.left.left = new TreeNode(4);
+		root1.left.left.right = new TreeNode(30);
+		root1.left.right = new TreeNode(6);
+		
+		TreeNode root2 = new TreeNode(10);
+		root2.right = new TreeNode(6);
+		root2.left  = new TreeNode(4);
+		root2.left.right = new TreeNode(30);
+		
+		if(tree.isSubTree(tree.root1,tree.root2))
+			System.out.println("Tree2 is subtree of Tree1");
+		else
+			System.out.println("Tree2 is not a subtree of Tree1");
+		
+	}
+
+}
