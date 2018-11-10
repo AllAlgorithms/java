e16aded742917bceb62bad755e4716e3a88d9f37
Abraham Hernandez <abraham@abranhe.com>
2018-11-10 00:37:08 -0500

    Update Fibonacci.java

--- dynamic-programming/Fibonacci.java
+++ dynamic-programming/Fibonacci.java
@@ -1,19 +1,33 @@
-class Fibonacci {
-    static int fib(int n) {
-        int dp[] = new int[n+2];
-
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-
-        return dp[n];
-    }
-
-    public static void main (String args[]) {
-        int n = 10;
-        System.out.println(fib(n));
-    }
+/**
+ * Fibonacci Series using Dynamic Programming 
+ *
+ * @author mokshagna517
+ */
+class fibonacci 
+{ 
+   static int fib(int n) 
+    { 
+    /* Declare an array to store Fibonacci numbers. */
+    int f[] = new int[n+2]; // 1 extra to handle case, n = 0 
+    int i; 
+       
+    /* 0th and 1st number of the series are 0 and 1*/
+    f[0] = 0; 
+    f[1] = 1; 
+      
+    for (i = 2; i <= n; i++) 
+    { 
+       /* Add the previous 2 numbers in the series 
+         and store it */
+        f[i] = f[i-1] + f[i-2]; 
+    } 
+       
+    return f[n]; 
+    } 
+       
+    public static void main (String args[]) 
+    { 
+        int n = 9; 
+        System.out.println(fib(n)); 
+    } 
 } 
