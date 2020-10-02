04119852a809326f1089e05505dd975ff0946dc2
nishantmovaliya <63806205+nishantmovaliya@users.noreply.github.com>
2020-10-02 19:17:28 +0530

    Update LinearSearch.java
    add main method

--- algorithms/searches/LinearSearch.java
+++ algorithms/searches/LinearSearch.java
@@ -4,7 +4,7 @@
 
 class LinearSearch {
     // This function returns index of element x in arr[]
-    static int search(int arr[], int n, int x) {
+     int search(int arr[], int n, int x) {
         for (int i = 0; i < n; i++) {
             // Return the index of the element if the element
             // is found
@@ -16,4 +16,18 @@ class LinearSearch {
         return -1;
     }
 
+
+
+        public static void main(String args[]) {
+        LinearSearch ob = new LinearSearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.search(arr,n, x);
+        
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at " + "index " + result);
+ }
 }
