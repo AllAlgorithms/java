44737f8f0be9ceec4387e220a0244cb9658fc5b8
Jan Tabacki <jan.tabacki@gmail.com>
2020-10-07 11:29:53 +0200

    + implementation of generic InsertionSort.java

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,16 @@
+public class InsertionSort<T extends Comparable<? super T>> {
+    public T[] Sort(T[] values) {
+        if (values.length > 0) {
+            T value = values[0];
+            for (int i = 1; i < values.length; ++i) {
+                value = values[i];
+                int j;
+                for (j = i - 1; j >= 0 && values[j].compareTo(value) > 0; --j) {
+                    values[j + 1] = values[j];
+                }
+                values[j + 1] = value;
+            }
+        }
+        return values;
+    }
+}
